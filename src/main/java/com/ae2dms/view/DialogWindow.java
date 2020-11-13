695a74f3841cc7f9e87aed2825e8512fbd75760b
scyyf1 <scyyf1@nottingham.edu.cn>
2020-11-13 23:19:45 +0800

    refactor(Main.newDialog): store the hard code of dialog setting in class DialogWindow

---
+++ src/main/java/com/ae2dms/view/DialogWindow.java
@@ -0,0 +1,59 @@
+package com.ae2dms.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.effect.Effect;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * <p>
+ * Package: com.ae2dms.view
+ *
+ * @description: The view of new dialog window without dialog message effect
+ * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+ * @date: 2020/11/13 22:33
+ * @version: 1.0
+ */
+public class DialogWindow {
+    private Stage dialogWindow;
+
+    /**
+     * constructor
+     *
+     * @param primaryStage
+     * @param dialogTitle
+     * @param dialogMessage
+     * @description: construct a instance of dialog window without visually present
+     * refactor from Main.java.newDialog
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/11/13 23:16
+     * @version: 1.0.0
+     **/
+
+    public DialogWindow(Stage primaryStage, String dialogTitle, String dialogMessage) {
+        Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(primaryStage);
+        dialog.setResizable(false);
+        dialog.setTitle(dialogTitle);
+
+        Text text1 = new Text(dialogMessage);
+        text1.setTextAlignment(TextAlignment.CENTER);
+        text1.setFont(javafx.scene.text.Font.font(14));
+        this.dialogWindow = dialog;
+
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.setAlignment(Pos.CENTER);
+        dialogVbox.setBackground(Background.EMPTY);
+        dialogVbox.getChildren().add(text1);
+
+        Scene dialogScene = new Scene(dialogVbox, 350, 150);
+        dialog.setScene(dialogScene);
+    }
+}
