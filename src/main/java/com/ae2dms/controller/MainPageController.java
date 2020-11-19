d9ba0be2ebdc52141732dfd5d0ed4ec220659f91
scyyf1 <scyyf1@nottingham.edu.cn>
2020-11-19 23:11:12 +0800

    docs(main page): add main page/starting page of this game in MVC pattern

---
+++ src/main/java/com/ae2dms/controller/MainPageController.java
@@ -0,0 +1,42 @@
+package com.ae2dms.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * <p>
+ * Package: com.ae2dms.controller
+ *
+ * @description: This is the class to control the main page
+ * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+ * @date: 2020/11/9 21:10
+ */
+public class MainPageController {
+    public Button startButton;
+    public Button aboutButton;
+
+    /**
+     * @param mouseEvent
+     * @return void
+     * @description start game when the start button is clicked
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/11/9 22:14
+     **/
+
+
+    public void onStartClicked(MouseEvent mouseEvent) {
+    }
+
+    /**
+    * @description new dialog for about information
+    * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+    * @date: 2020/11/9 22:15
+     * @param mouseEvent
+    * @return void
+    **/
+
+
+    public void onAboutClicked(MouseEvent mouseEvent) {
+    }
+}
