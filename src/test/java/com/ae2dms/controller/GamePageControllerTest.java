9862d269f2adb060b24bd9a2c765037370332497
scyyf1 <scyyf1@nottingham.edu.cn>
2020-11-25 17:18:56 +0800

    test(GameEngineTest, GamePageControllerTest): add test class for two classes GameEngine and GamePageController. The MAVEN configuration file pom.xml is modified to include JUnit test library.

---
+++ src/test/java/com/ae2dms/controller/GamePageControllerTest.java
@@ -0,0 +1,63 @@
+package com.ae2dms.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * <p>
+ * Package: com.ae2dms.controller
+ *
+ * @description: TODO
+ * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+ * @date: 2020/11/25 12:22
+ */
+class GamePageControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void saveGame() {
+    }
+
+    @Test
+    void loadGame() {
+    }
+
+    @Test
+    void closeGame() {
+    }
+
+    @Test
+    void undo() {
+    }
+
+    @Test
+    void toggleMusic() {
+    }
+
+    @Test
+    void toggleDebug() {
+    }
+
+    @Test
+    void resetLevel() {
+    }
+
+    @Test
+    void showAbout() {
+    }
+}
