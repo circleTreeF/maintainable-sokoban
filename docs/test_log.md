8011d64bf8780e1f263e5cf49c8ccf048f345d63
scyyf1 <scyyf1@nottingham.edu.cn>
2020-11-26 15:58:35 +0800

    test(GameEngine): test method `undo` in the class GameEngine. The first test is failed while the contents are identical. The second test is passed, by changing the comparison between reference of the objects to the string content of the objects. The test is logged in test_log.md.
    Close-issue: issue #5 and issue #20

---
+++ docs/test_log.md
@@ -0,0 +1,17 @@
+## GameEngineTest
+
+### undo()
+
+In this test, the map file debugGame.skb would be applied to test.
+
+#### first test
+
+**Test fails** while the contents are identical
+
+![](img/images_test_GameEngineTest_undo_1)
+
+### second test
+
+assert the string of `objectsGrid` and `diamondsGrid` in the `currentLevel` of the instance of  `GameEngine` by applying function `toString` on all objects.
+
+**Test Pass**
---
+++ src/test/main/java/com/ae2dms/model/GameEngineTest.java
@@ -0,0 +1,68 @@
+package com.ae2dms.model;
+
+import com.ae2dms.controller.GamePageController;
+import javafx.scene.input.KeyCode;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * <p>
+ * Package: com.ae2dms.model
+ *
+ * @description: This class is the test class for the class GameEngine in the model package of this project
+ * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+ * @date: 2020/11/25 14:35
+ */
+class GameEngineTest {
+
+    @Test
+    void isDebugActive() {
+        InputStream inputStream = GameEngineTest.class.getClassLoader().getResourceAsStream("level/debugGame.skb");
+        GameEngine gameEngine = new GameEngine(inputStream, true);
+        assertFalse(GameEngine.isDebugActive());
+    }
+
+    @Test
+    void handleKey() {
+
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void isGameComplete() {
+    }
+
+    @Test
+    void getNextLevel() {
+    }
+
+    @Test
+    void getCurrentLevel() {
+    }
+
+    @Test
+    void toggleDebug() {
+    }
+
+    @Test
+    void undo() {
+        InputStream inputStream = GameEngineTest.class.getClassLoader().getResourceAsStream("level/debugGame.skb");
+        GameEngine gameEngine = new GameEngine(inputStream, true);
+        gameEngine.handleKey(KeyCode.LEFT);
+        gameEngine.undo();
+        assertEquals(gameEngine.movesCount,1);
+
+        InputStream sameInputStream = GamePageController.class.getClassLoader().getResourceAsStream("level/debugGame.skb");
+        GameEngine sameGameEngine = new GameEngine(sameInputStream, true);
+        assertEquals(sameGameEngine.getCurrentLevel().objectsGrid.toString(), gameEngine.getCurrentLevel().objectsGrid.toString());
+        assertEquals(sameGameEngine.getCurrentLevel().diamondsGrid.toString(),gameEngine.getCurrentLevel().diamondsGrid.toString());
+    }
+}
