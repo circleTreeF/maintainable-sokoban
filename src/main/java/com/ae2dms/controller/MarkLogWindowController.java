3d656668f4061118392fe4b543760cb1d6a9fa23
scyyf1 <scyyf1@nottingham.edu.cn>
2020-12-04 00:59:39 +0800

    docs(MarkLogWindowController): add Javadoc for class MarkLogWindowController

--- src/main/java/com/ae2dms/controller/MarkLogWindowController.java
+++ src/main/java/com/ae2dms/controller/MarkLogWindowController.java
@@ -6,7 +6,6 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -24,7 +23,7 @@ import static com.ae2dms.Main.gameEngine;
  * <p>
  * Package: com.ae2dms.controller
  *
- * @description: TODO
+ * @description: This class is the controller to the view page, mark log window. This page will pop up when the user completed a certain game map
  * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
  * @date: 2020/12/2 21:31
  */
@@ -36,6 +35,16 @@ public class MarkLogWindowController {
     private String mapName;
     private Integer movesCount;
 
+    /**
+     * initialize the game completion page or the mark log page. Present the completed map name, the moves count to complete this map, and text field to prompt user to enter a user name
+     *
+     * @param
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 0:44
+     * @version: 1.0.0
+     **/
+
     public void initialize() {
         mapName = gameEngine.mapSetName;
         movesCount = gameEngine.previousLevelsMovesCountsProperty.getValue();
@@ -43,8 +52,17 @@ public class MarkLogWindowController {
         movesCountMessage.setText(String.valueOf(movesCount));
     }
 
+    /**
+     * save the user's game record on the specific map {@code gameEngine.mapSetName}, the moves count {@code movesCount}, and the user-inputted user name {@code String userName = inputName.getText()} in the serialized file, ranking.json in the rank directory of project resources path.
+     *
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 0:46
+     * @version: 1.0.0
+     **/
 
-    public void onSaveAndBackClicked(MouseEvent mouseEvent) throws IOException {
+
+    public void onSaveAndBackClicked() throws IOException {
         String userName = inputName.getText();
         if (GameEngine.isDebugActive()) {
             System.out.println(userName);
@@ -55,7 +73,7 @@ public class MarkLogWindowController {
         FileReader fileReader = new FileReader(String.valueOf(getClass().getClassLoader().getResource("rank/ranking.json").getFile()));
         ArrayList<MarkKeeper> markKeeper = gson.fromJson(fileReader, REVIEW_TYPE);
         fileReader.close();
-        if (markKeeper==null){
+        if (markKeeper == null) {
             markKeeper = new ArrayList<MarkKeeper>();
         }
         markKeeper.add(new MarkKeeper(mapName, userName, movesCount));
