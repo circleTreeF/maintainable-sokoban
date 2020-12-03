e0862db1bd1ce4cf88e555cc9cc75d54c1682293
scyyf1 <scyyf1@nottingham.edu.cn>
2020-12-04 01:37:24 +0800

    docs(MarkBoardController): add Javadoc for methods in new class MarkBoardController. This is the class to controller the scene of mark board.
     Realted issue #34

--- src/main/java/com/ae2dms/controller/MarkBoardController.java
+++ src/main/java/com/ae2dms/controller/MarkBoardController.java
@@ -24,7 +24,7 @@ import java.util.List;
  * <p>
  * Package: com.ae2dms.controller
  *
- * @description: TODO
+ * @description: This class includes controller for {@code MarkBoard.fxml}, which is the view page for the game history record
  * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
  * @date: 2020/12/3 18:15
  */
@@ -37,10 +37,23 @@ public class MarkBoardController {
     private String DEFAULT_MAP_NAME = "Example Game!";
     private final int NUMBER_OF_SHOWN_ITEM = 5;
 
+    /**
+     * initialize the mark board page. This page will present the game records and combobox for map selection. By default, the default map records will be present.
+     * <p>If there is not any record of any map of this sokoban game, the warning text, "No Record" will be present </p>
+     *
+     * @param
+     * @return void
+     * @throws IOException
+     *         Signals that an I/O exception of some sort has occurred
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 1:01
+     * @version:
+     **/
+
     public void initialize() throws IOException {
         readMarks();
         initializeComboBox();
-        if (markKeepers!=null){
+        if (markKeepers != null) {
             mapSelectionComboBox.getSelectionModel().selectedItemProperty().addListener(
                     new ChangeListener<String>() {
                         @Override
@@ -50,25 +63,50 @@ public class MarkBoardController {
                     }
             );
             setMarkBoardForMap(DEFAULT_MAP_NAME);
-        }else {
+        } else {
             noRecordPrompt.visibleProperty().setValue(true);
         }
 
     }
 
+
+    /**
+     * read the users' game record stored in the serialized file, ranking.json in the rank directory of project resources path.
+     *
+     * @return void
+     * @throws IOException
+     *         Signals that an I/O exception of some sort has occurred
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 1:08
+     * @version: 1.0.0
+     **/
+
     private void readMarks() throws IOException {
-        Type REVIEW_TYPE = new TypeToken<List<MarkKeeper>>() {
+        Type reviewType = new TypeToken<List<MarkKeeper>>() {
         }.getType();
         Gson gson = new Gson();
         FileReader fileReader = new FileReader(String.valueOf(getClass().getClassLoader().getResource("rank/ranking.json").getFile()));
-        markKeepers = gson.fromJson(fileReader, REVIEW_TYPE);
+        markKeepers = gson.fromJson(fileReader, reviewType);
         fileReader.close();
     }
 
 
+    /**
+     * initialize the combo box of this mark board view page.
+     * <p>If there is map record in the ranking file, all available map name stored in the ranking file will be present in this node.</p>
+     * <p>Otherwise, the combo box will be disabled.</p>
+     *
+     * @param
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 1:09
+     * @version: 1.0.0
+     **/
+
+
     private void initializeComboBox() {
         ObservableList<String> comboBoxOptions = FXCollections.observableArrayList();
-        if (markKeepers == null){
+        if (markKeepers == null) {
             mapSelectionComboBox.setDisable(true);
             return;
         }
@@ -79,6 +117,19 @@ public class MarkBoardController {
         }
         mapSelectionComboBox.itemsProperty().setValue(comboBoxOptions);
     }
+
+    /**
+     * present the history record of map {@code String mapName}, in the grid pane of this page. The record will be present in the follow order, user name, and moves counts.
+     * <p>If the number of records of this map is more than 5, only 5 record with the smallest moves counts will be present.</p>
+     * <p>Otherwise, all records of this map will be present.</p>
+     *
+     * @param mapName
+     *         the name of map to be present in this scene
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 1:12
+     * @version:
+     **/
 
 
     private void setMarkBoardForMap(String mapName) {
@@ -97,11 +148,27 @@ public class MarkBoardController {
         }
         for (int count = 0; count < shownMarkKeepersForMap.size(); count++) {
             MarkKeeper markKeeperContent = shownMarkKeepersForMap.get(count);
-            addTextToGrid(markKeeperContent.userName, 1, count+1);
-            addTextToGrid(String.valueOf(markKeeperContent.movesCount),2, count+1);
+            addTextToGrid(markKeeperContent.userName, 1, count + 1);
+            addTextToGrid(String.valueOf(markKeeperContent.movesCount), 2, count + 1);
         }
 
     }
+
+
+    /**
+     * add the history record in {@code Text} to the grid pane of this scene
+     *
+     * @param text
+     *         the {@code String} of the history records
+     * @param column
+     *         the column of this text should be present in the grid pane
+     * @param row
+     *         the row of this text should be present in the grid pane
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/4 1:15
+     * @version:
+     **/
 
     private void addTextToGrid(String text, int column, int row) {
         Text textContent = new Text(text);
