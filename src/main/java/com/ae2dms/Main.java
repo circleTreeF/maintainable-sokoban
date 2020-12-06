15320a440869ca39af45b23f32cf52893fb91896
scyyf1 <scyyf1@nottingham.edu.cn>
2020-12-07 06:01:40 +0800

    docs(main): Javadoc for main

--- src/main/java/com/ae2dms/Main.java
+++ src/main/java/com/ae2dms/Main.java
@@ -8,19 +8,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * This is the main class of the whole project. The project of AE2DMS Coursework of Yizirui FANG 20127091
  * <p>
  * Package: com.ae2dms.
  *
- * @description: This is the main class
  * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
  * @date: 2020/11/9 21:10 given
  * @version: 1.0
  */
 public class Main extends Application {
 
+    /**
+     * The stage this game is to present at
+     */
     public static Stage primaryStage;
+    /**
+     * The instance of the current game
+     */
     public static GameEngine gameEngine;
+
+    /**
+     * The main method to start this project
+     *
+     * @param args
+     *         any input arguments of this project
+     * @return void
+     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+     * @date: 2020/12/6 16:55
+     * @version: 1.0.0
+     **/
 
     public static void main(String[] args) {
         launch(args);
@@ -30,22 +46,14 @@ public class Main extends Application {
      * @param primaryStage
      *         the primary stage of this game page scene
      * @return void
-     * @description: set the to menu of the initial interface of the game
-     * In the menu, 3 options are "file", "level", "about"
-     * * Under each option, there are menu items.
-     * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
-     * @date: 2020/11/10 14:11 given
-     * @version: 1.0.0
-     **/
-    /**
-     * @param primaryStage
-     *         the primary stage of this game page scene
-     * @return void
+     * @throws Exception
+     *         This indicates the general exception in this project
      * @description: show the game page of this game by load the view from fxml file in view package
      * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
      * @date: 2020/11/19 21:47
      * @version: 2.0.0
      **/
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
