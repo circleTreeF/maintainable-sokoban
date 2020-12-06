adbf0321f3c3574430875d3b26fc52e8f491401e
scyyf1 <scyyf1@nottingham.edu.cn>
2020-12-06 18:30:30 +0800

    test(GameGrid): test key methods in class `GameGrid`. All test finally passes.

---
+++ src/test/main/java/com/ae2dms/model/GameGridTest.java
@@ -0,0 +1,65 @@
+package com.ae2dms.model;
+
+import com.ae2dms.GameObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The project of AE2DMS Coursework of Yizirui FANG 20127091
+ * <p>
+ * Package: com.ae2dms.model
+ * <p>
+ * This class is to test the class GameGrid
+ *
+ * @author: Yizirui FANG ID: 20127091 Email: scyyf1@nottingham.edu.cn
+ * @date: 2020/12/6 17:38
+ */
+class GameGridTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void translatePoint() {
+        assertEquals(new Point(11, 11), GameGrid.translatePoint(new Point(10, 10), new Point(1, 1)));
+    }
+
+    @Test
+    void getTargetFromSource() {
+        GameGrid gameGrid = new GameGrid(20, 20);
+        gameGrid.putGameObjectAt(GameObject.FLOOR, 10, 10);
+        assertEquals(GameObject.FLOOR, gameGrid.getGameObjectAt(10, 10));
+        assertEquals(GameObject.FLOOR, gameGrid.getTargetFromSource(new Point(9,9), new Point(1,1)));
+    }
+
+    @Test
+    void getGameObjectAt() {
+        GameGrid gameGrid = new GameGrid(20, 20);
+        gameGrid.putGameObjectAt(GameObject.FLOOR, 10, 10);
+        assertEquals(GameObject.FLOOR, gameGrid.getGameObjectAt(new Point(10,10)));
+    }
+
+    @Test
+    void putGameObjectAt() {
+        GameGrid gameGrid = new GameGrid(20, 20);
+        gameGrid.putGameObjectAt(GameObject.FLOOR, 10, 10);
+        assertEquals(GameObject.FLOOR, gameGrid.getGameObjectAt(10, 10));
+    }
+
+    @Test
+    void testPutGameObjectAt() {
+        GameGrid gameGrid = new GameGrid(20, 20);
+        gameGrid.putGameObjectAt(GameObject.FLOOR, new Point(12, 10));
+        assertEquals(GameObject.FLOOR, gameGrid.getGameObjectAt(12, 10));
+    }
+}
